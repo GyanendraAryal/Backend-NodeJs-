0930b65534ebc7c949b8988bd756e42e81ef6444
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-01-01 21:40:53 +0545

    array

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-console.log("Hello World!!");
+//console.log("Hello World!!");
 //console.log(5+2);
 console.log("Welcome to NodeJs!!");
-const a = 4;
-const b = 4;
+//const a = 4;
+//const b = 4;
 const name = "Gyanendra"
 let surname = " Aryal"
 //console.log("The value of a and b are:"+a);
-console.log("The sum of a and b is:"+(a+b));
+//console.log("The sum of a and b is:"+(a+b));
 console.log("The name of this project owner is:"+(name+surname));
+
+//Array
+
+let arr = ["1", "Val2", 2 ];
+console.log("Value in array position 0 and 2 are " +arr[0],arr[2])
