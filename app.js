123180d8737c5f7b4f6d9883b2068b5121d551e2
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-01-01 21:11:17 +0545

    string concat

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 console.log("Hello World!!");
-console.log(5+2);
+//console.log(5+2);
 console.log("Welcome to NodeJs!!");
 const a = 4;
 const b = 4;
-console.log("The value of a and b are:"+a);
+const name = "Gyanendra"
+let surname = " Aryal"
+//console.log("The value of a and b are:"+a);
 console.log("The sum of a and b is:"+(a+b));
+console.log("The name of this project owner is:"+(name+surname));
