eef57fb2f34f71ac1246af4c9e59fdaed5e20141
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-01-01 21:06:56 +0545

    variables and console

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("Hello World!!");
+console.log(5+2);
+console.log("Welcome to NodeJs!!");
+const a = 4;
+const b = 4;
+console.log("The value of a and b are:"+a);
+console.log("The sum of a and b is:"+(a+b));
