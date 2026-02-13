c942156da2322386510cc43106a2fb72443c17ca
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-02-13 22:22:18 +0545

    created hello world file for nodejs

---
+++ HelloWorld/helloworld.js
@@ -0,0 +1,3 @@
+console.log("Hello World!!");
+//console.log(window);  //window and dom objects are not availabe in v8 engine in nodejs
+//console.log(alert('Hey'));//Only core functions of javascript that are needed in server side are availabe in the nodejs v8 engine 
