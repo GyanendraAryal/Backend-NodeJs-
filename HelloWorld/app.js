b16103e8632373da5911019a965b7bd60e63fcb6
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-02-14 19:49:16 +0545

    created app.js and practiced modular import and export

---
+++ HelloWorld/app.js
@@ -0,0 +1,4 @@
+const {add, sub} = require('./helloworld');//Importing the functions by destructuring
+
+console.log(add(5,3));//8
+console.log(sub(7,2));//5
--- HelloWorld/helloworld.js
+++ HelloWorld/helloworld.js
@@ -1,3 +1,20 @@
 console.log("Hello World!!");
 //console.log(window);  //window and dom objects are not availabe in v8 engine in nodejs
 //console.log(alert('Hey'));//Only core functions of javascript that are needed in server side are availabe in the nodejs v8 engine 
+
+const add = (a,b) => {
+    return a+b;
+}
+
+const sub = (a,b) => {
+    return a-b;
+}
+
+module.exports = {
+    add,
+    sub
+};
+//Exporting as a object because of multiple functions to be exported
+//module.exports can be used only once in a file
+//module.exports = add; (wrong)
+//module.exports = sub; (wrong)
