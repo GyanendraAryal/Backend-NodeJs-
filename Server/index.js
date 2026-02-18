4c884c3afc51958b46714d1a65180372a1cb702b
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-02-18 20:41:59 +0545

    modified server

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,23 @@
 const http = require('http');
+const fs = require('fs');
 const Port = 8000;
 
 const myServer = http.createServer((req,res)=>{
-    console.log("New request received");
-    res.end('Hello from server');
+    const log = `${Date.now()}: ${req.url} Another request received\n`
+    fs.appendFile('log.txt',log,(err,result)=>{
+       switch(req.url){
+        case '/': res.end("I'm from Home Page");
+        break;
+        case '/about': res.end("I'm from About Page");
+        break;
+        case '/contact': res.end("I'm from Contact Page");
+        break;
+        default: res.end('404-Page Not Found')
+       }
+    })
+
+    //console.log(req);
+    //console.log(req.headers);    //console.log("New request received");
 });
 
 myServer.listen(Port,()=>{
