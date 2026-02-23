2d7761764d188bf7100ea2967d7b50875aa4a44c
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-02-23 16:01:20 +0545

    practiced middlewares in express/nodejs

---
+++ Middleware/index.js
@@ -0,0 +1,54 @@
+import express from "express";
+import users from "./MOCK_DATA.json" with { type: "json" }
+
+
+const app = express();
+const PORT = 8000;
+
+//Middlewares
+app.use(express.urlencoded({ extended: false }));//It allows your server to read form data sent from HTML forms.
+
+app.use((req,res, next) =>{
+    console.log("Hello from Middleware 1");
+    res.end("Hello from middleware");
+    next();
+    
+})
+
+app.get("/", (req, res) => {
+    return res.send("Hello I'm from the Home Page!");
+})
+
+app.get("/users", (req, res) => {
+    const html =
+        `<ul>
+            ${users.map((users) =>{ 
+            return `<li>
+                    ${users.first_name}
+                </li>
+                <li>
+                    ${users.email}
+                </li>`
+            }).join("")}
+    </ul>`
+
+    return res.send(html);
+})
+
+app.get("/api/users/:id", (req, res) => {
+
+    const userId = Number(req.params.id);
+    const user = users.find(u => u.id === userId);
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+
+})
+
+
+
+app.listen(PORT, () => {
+    console.log("Server running on port: ", PORT);
+})
