0a38b7e2ec5b6042779ecec59b1172f8b6968841
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-02-23 16:00:48 +0545

    improved index.js

--- API/index.js
+++ API/index.js
@@ -1,8 +1,18 @@
 import express from "express";
-import users from "./MOCK_DATA.json" with { type: 'json'}
+import users from "./MOCK_DATA.json" with { type: 'json'};
+import fs from 'fs';
 
 const app = express();
 const PORT = 8000;
+
+//Middleware Plugin
+//app.use(express.urlencoded({ extended : false }));
+
+app.use((req, res, next) =>{
+    console.log("Hello from middleware");
+    return res.json("Hello from middleware")
+    next();
+})
 
 
 //Routes
