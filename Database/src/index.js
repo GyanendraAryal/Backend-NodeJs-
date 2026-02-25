33b0bce25eb529530169c17aac242e50443a7dba
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-02-25 18:04:22 +0545

    Refactor index.js configuration

--- Database/src/index.js
+++ Database/src/index.js
@@ -1,12 +1,21 @@
 import connectDB from "./db/db.js";
 import dotenv from "dotenv"
+import app from "./app.js";
 
 
 dotenv.config({
-    path:'./.env'
+    path: './.env'
 });
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port: ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("MongoDB connection failed: ❌", err)
+    })
 
 
 
