cc2f6d2d2439fc299edb00cf6b16248e546ecf0c
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-02-25 18:04:31 +0545

    Add utils folder with helper functions

---
+++ Database/src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (requestHandler) =>{
+            (req,res,next) =>{
+                Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+            }
+}
+
+
+
+
+export default asyncHandler;
+
+//Using TryCatch
+/* const asyncHandler = (fn) => async (req,res,next) =>{
+    try {
+        await fn(req,res,next);
+    } catch (error) {
+        res.status(error.code||500).json({
+            sucess:false,
+            message:error.message
+        })
+    }
+}
+export default asyncHandler; */
