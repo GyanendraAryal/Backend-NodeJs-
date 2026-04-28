1723ee30d65e46784784844805789a9f876300c9
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-04-28 19:55:00 +0545

    chore: remove app.js

--- KGCoding/firstnodeserver/app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-let server = http.createServer((req,res) =>{
-    console.log(req);
-});
-server.listen(3000,() =>{
-    console.log("Server listening on port: ",3000);
-    
-})
